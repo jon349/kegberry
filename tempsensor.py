a9b3ed01d13ec517daca408b1edb28a71773aba4
jon349 <77199413+jon349@users.noreply.github.com>
2021-02-28 16:51:13 -0500

    Update tempsensor.py

--- tempsensor.py
+++ tempsensor.py
@@ -36,10 +36,12 @@ def read_temp_raw():
 # read_temp ====================================================================================================================	
 def read_temp():
     lines = read_temp_raw()
+    print(lines)
     if lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
+    print(equals_pos)
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
