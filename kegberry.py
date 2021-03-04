f8e150b34cf17b7a28ae109c3fe1cf390f445e70
jon349 <77199413+jon349@users.noreply.github.com>
2021-03-04 18:55:11 -0500

    Update kegberry.py

--- kegberry.py
+++ kegberry.py
@@ -280,7 +280,7 @@ def renderThings(flowMeter1, flowMeter2, flowMeter3, screen,
 	# Using Pin 4
 	# Left Justified	
 	screenfont = pygame.font.SysFont(None, 35)
-	if read_temp(self) is not None:
+	if read_temp() is not None:
 		print(type(read_temp()))
 		rendered = screenfont.render("Kegerator Temp: " + str(round(read_temp(),1)) + " F", True, WHITE, BLACK)
 		screen.blit(rendered, ((VIEW_WIDTH - rendered.get_rect().width), 575))
