768e577337b36e2e80f501357f1d361db14422a7
jon349 <77199413+jon349@users.noreply.github.com>
2021-03-05 18:25:04 -0500

    Update tempsensor.py

--- tempsensor.py
+++ tempsensor.py
@@ -45,5 +45,5 @@ def read_temp():
 			temp_string = lines[1][equals_pos+2:]
 			temp_c = float(temp_string) / 1000.0
 			temp_f = temp_c * 9.0 / 5.0 + 32.0
-			return temp_f  # add/change temp_c
 			print(temp_f)
+			return temp_f  # add/change temp_c
