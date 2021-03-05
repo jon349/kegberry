65b5c5d1e53b819b793b033f37771a5608217933
jon349 <77199413+jon349@users.noreply.github.com>
2021-03-05 18:50:53 -0500

    Update tempsensor.py

--- tempsensor.py
+++ tempsensor.py
@@ -36,13 +36,13 @@ def read_temp_raw():
 # read_temp ====================================================================================================================	
 def read_temp():
 	lines = read_temp_raw()
-	if lines is not None:
-		if len(lines) != 0:
-			if lines[0].strip()[-3:] != 'YES':
-				time.sleep(0.2)
-				lines = read_temp_raw()
-			equals_pos = lines[1].find('t=')
-			if equals_pos != -1:
+	if len(lines) != 0:
+		if lines[0].strip()[-3:] != 'YES':
+			time.sleep(0.2)
+			lines = read_temp_raw()
+		equals_pos = lines[1].find('t=')
+		if equals_pos != -1:
+			if lines is not None
 				temp_string = lines[1][equals_pos+2:]
 				temp_c = float(temp_string) / 1000.0
 				temp_f = temp_c * 9.0 / 5.0 + 32.0
